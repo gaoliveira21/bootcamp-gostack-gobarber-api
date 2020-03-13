a4ec2a1d1cb277e496ef2cab8422594c3f46bc95
Gabriel José <gabriel.oliveira@liveonsolutions.com>
2020-03-13 16:12:32 -0300

    password hash

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -6,11 +7,20 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // não existe no bd
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
-      { sequelize }
+      { sequelize } // variavel de conexão
     );
+    // hook chamado antes de criar/atualizar usuário
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
