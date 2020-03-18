8c68cc36f0717970a24c71cd3f1504e80ef0aeab
Gabriel José <gabriel.oliveira@liveonsolutions.com>
2020-03-18 17:03:38 -0300

    mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '033638c1b27255',
+    pass: 'bae3cb58f80e99',
+  },
+  default: {
+    to: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
