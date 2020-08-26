07b763ac993428899e49fd1f30f1c13f40e931f3
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-08-26 15:58:49 -0300

    abstract validators

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,20 +4,6 @@ import File from '../models/File';
 
 class UserController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string()
-        .required()
-        .min(6),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
-    }
-
     const usersExists = await User.findOne({
       where: { email: req.body.email },
     });
